1ccd14ae1a67a534c708abb1919c809201b18054
carlosbasiliotorres <carlos.basilio@outlook.es>
2021-03-13 19:07:28 -0600

    correccion de archivos

--- index.js
+++ index.js
@@ -1,21 +1,86 @@
-class Persona{
-    constructor(nombre,apellido,edad){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-    }
-}
+ const btnEmpezar = document.getElementById('btnEmpezar')
+ const ULTIMO_NIVEL = 10;
 
-var persona = new Persona("carlos","basilio",32);
+     class Juego {
+         constructor() {
+             this.inicializar()
+             this.generaPatron()
+         }
 
-/*const nombre = persona.nombre;
-const apellido = persona.apellido;
-const edad = persona.edad;
-*/
+         inicializar() {                         
+             this.elegirColor = this.elegirColor.bind(this)
+             btnEmpezar.classList.add('hide')
+             this.nivel = 0;
+             this.colores = [
+                 document.getElementById('celeste'),
+                 document.getElementById('violeta'),
+                 document.getElementById('naranja'),
+                 document.getElementById('verde')
+             ]
+         }
+
+         generaPatron() {
+             this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+             this.siguienteNivel();
+         }
+
+         siguienteNivel() {
+             this.nivel++;
+             this.subnivel = 0;
+             this.iluminarSecuencia()
+             this.buscarClick()
+         }
+
+         iluminarSecuencia() {
+             setTimeout(() => {
+                 for (var i = 0; i < this.nivel; i++) {
+                     let color = this.colores[this.secuencia[i]];
+                     setTimeout(() => this.iluminarColor(color), 1000 * i)
+                 }
+             }, 1000);
+         }
+
+         iluminarColor(color) {
+             color.classList.add("light")
+             setTimeout(() => this.apagarColor(color), 350)
+         }
+
+         apagarColor(color) {
+             color.classList.remove("light")
+         }
 
 
-const {nombre,apellido,edad} = persona;
+         buscarClick() {
+             this.colores[0].addEventListener('click', this.elegirColor)
+             this.colores[1].addEventListener('click', this.elegirColor)
+             this.colores[2].addEventListener('click', this.elegirColor)
+             this.colores[3].addEventListener('click', this.elegirColor)
+         }
 
-console.log(nombre)
-console.log(apellido)
-console.log(edad)
+         elegirColor(e) {
+             this.iluminarColor(e.target)
+             if (Number(this.colores.indexOf(e.target)) === Number(this.secuencia[this.subnivel])) {
+                 this.subnivel++;                 
+                 if (this.subnivel == this.nivel) {
+                    if(this.nivel === ULTIMO_NIVEL) {
+                        swal("Felicidades!!!!","Has concluido el desafio y el juego ha terminado","success")
+                        .then(()=>{
+                            location.reload();
+                        })
+                    }else{
+                        this.siguienteNivel();
+                    }
+                 }
+             }else{
+                swal("Lo sentimos","Has perdido y el juego ha terminado","error")
+                .then(()=>{
+                    location.reload();
+                })
+             }
+         }
+
+     }
+
+ function empezarJuego() {
+     var juego = new Juego()     
+ }
